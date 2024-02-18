8fb2951f79ba096b39dac9ac4cc0907ea1ab1af8
KIT <nikita002005@gmail.com>
2024-02-18 21:36:45 +0300

    First algo task

--- gg.go
+++ gg.go
@@ -2,19 +2,47 @@ package main
 
 import (
 	"fmt"
+	"math/rand"
 )
-// Add handler for input data
-func HandlerInputData(data *int) {
-	if *data % 2 == 0{
-		fmt.Println("Number is even")
-	}else {
-		fmt.Println("Number is odd")
+
+func quickSort(ar []int) {
+	if len(ar) < 2 {
+		return
 	}
+
+	left, right := 0, len(ar) - 1
+	pivotIndex := rand.Int() % len(ar)
+
+	ar[pivotIndex], ar[right] = ar[right], ar[pivotIndex]
+
+	for i := 0; i < len(ar); i++ {
+		if ar[i] < ar[right] {
+			ar[i], ar[left] = ar[left], ar[i]
+			left++
+		}
+	}
+
+	ar[left], ar[right] = ar[right], ar[left]
+
+	quickSort(ar[:left])
+	quickSort(ar[left + 1:])
+
+	return
+}
+
+// create func to join array1 and array2
+func merge(num1, nums2 []int) {
+	for k,i := range nums2 {
+		num1[k+3] = i
+	}
+	quickSort(num1)
 }
 
 func main() {
-	fmt.Println("Hello world")
-	var c int // my number
-	fmt.Scanln(&c)
-	fmt.Printf("my number is: %d", c)
+	// Arrays
+	nums1 := []int{1, 2, 3, 0, 0, 0}
+	nums2 := []int{2, 5, 6}
+	
+	merge(nums1, nums2)
+	fmt.Println(nums1)
 }
