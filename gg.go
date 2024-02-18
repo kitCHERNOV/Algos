1cb2bea6846447c95f34af7cf48a0b8e73a3cf82
KIT <nikita002005@gmail.com>
2024-02-18 18:06:25 +0300

    v.2.2 Add handler for input data

--- gg.go
+++ gg.go
@@ -3,6 +3,14 @@ package main
 import (
 	"fmt"
 )
+// Add handler for input data
+func HandlerInputData(data *int) {
+	if *data % 2 == 0{
+		fmt.Println("number is even")
+	}else {
+		fmt.Println("number is odd")
+	}
+}
 
 func main() {
 	fmt.Println("Hello world")
